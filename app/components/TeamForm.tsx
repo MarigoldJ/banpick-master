6a13359d64ea39b915a7fb7f4cf9af1eb1fadae1
MarigoldJ <yusang0130@gmail.com>
2023-05-29 14:29:27 +0900

    feat: add submit func. connect input and button.

--- app/components/TeamForm.tsx
+++ app/components/TeamForm.tsx
@@ -1,16 +1,52 @@
 "use client";
 
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+
 import SubmitButton from "./SubmitButton";
 import TeamInput from "./TeamInput";
+import axios from "axios";
 
 const TeamForm = () => {
-  const onSubmit = () => {};
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      blueName: "",
+      redName: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    console.log(data);
+    // TODO: send data to DB.
+    setIsLoading(false);
+  };
 
   return (
     <div className="flex flex-col">
-      <TeamInput label="BLUE" color="blue" type="text" />
-      <TeamInput label="RED" color="red" type="text" />
-      <SubmitButton label="밴픽 시작하기" onClick={onSubmit} />
+      <TeamInput
+        id="blueName"
+        label="BLUE"
+        color="blue"
+        type="text"
+        register={register}
+        errors={errors}
+      />
+      <TeamInput
+        id="redName"
+        label="RED"
+        color="red"
+        type="text"
+        register={register}
+        errors={errors}
+      />
+      <SubmitButton label="밴픽 시작하기" onClick={handleSubmit(onSubmit)} />
     </div>
   );
 };
--- app/components/TeamInput.tsx
+++ app/components/TeamInput.tsx
@@ -1,12 +1,24 @@
 "use client";
 
+import { UseFormRegister, FieldValues, FieldErrors } from "react-hook-form";
+
 interface TeamInputProps {
+  id: string;
   label: string;
   color: "blue" | "red";
   type: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
 }
 
-const TeamInput: React.FC<TeamInputProps> = ({ label, color, type }) => {
+const TeamInput: React.FC<TeamInputProps> = ({
+  id,
+  label,
+  color,
+  type,
+  register,
+  errors,
+}) => {
   return (
     <div className="flex flex-row items-center ">
       <label
@@ -20,9 +32,15 @@ const TeamInput: React.FC<TeamInputProps> = ({ label, color, type }) => {
         {label}
       </label>
       <input
+        id={id}
+        {...register(id, { required: true })}
         placeholder=" "
         type={type}
-        className="p-4 m-2 border-2 rounded-md outline-none border-neutral-300 focus:border-neutral-500 transition"
+        className={`
+          p-4 m-2 border-2 rounded-md outline-none transition
+          ${errors[id] ? "border-rose-500" : "border-neutral-300"}
+          ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+        `}
       />
     </div>
   );
