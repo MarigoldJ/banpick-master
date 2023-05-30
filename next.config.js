fc8c9c0aa1f93c36bddd9632df9866a6dcb916e9
MarigoldJ <yusang0130@gmail.com>
2023-05-30 21:53:07 +0900

    chore: set for image load from riot api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "ddragon.leagueoflegends.com",
+        port: "",
+        pathname: "/cdn/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
