14d8326fb4886624fbb3cdb1e9b0593eab54aa66
MarigoldJ <yusang0130@gmail.com>
2023-05-30 21:52:28 +0900

    hotfix: avoid StaticGenBailoutError

--- app/hooks/useRiotApi.ts
+++ app/hooks/useRiotApi.ts
@@ -22,18 +22,24 @@ const useRiotApi = () => {
 
   // Get Champion List of Selected Patch Version
   const getChampList = async (version: string) => {
-    const res = await axios.get(`/api/riot/champ?ver=${version}`);
-    const result = res.data.data.data as Object;
-
-    return Object.values(result).sort((a: ChampType, b: ChampType) => {
-      if (a.name < b.name) {
-        return -1;
-      } else if (a.name > b.name) {
-        return 1;
-      } else {
-        return 0;
-      }
-    }) as Array<ChampType>;
+    // const res = await axios.get(`/api/riot/champ?ver=${version}`);
+    // const result = res.data.data.data as Object;
+    const res = await fetch(
+      `http://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`
+    );
+    return res.json().then((data) => {
+      return Object.values(data.data as Array<ChampType>).sort(
+        (a: ChampType, b: ChampType) => {
+          if (a.name < b.name) {
+            return -1;
+          } else if (a.name > b.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      ) as Array<ChampType>;
+    });
   };
 
   return {
