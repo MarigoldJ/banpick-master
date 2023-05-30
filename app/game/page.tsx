2eef28ed31807cf856ec8851b98d88df3d0887db
MarigoldJ <yusang0130@gmail.com>
2023-05-30 17:20:04 +0900

    feat: add basic components

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
 export default function Page() {
+  const params = useSearchParams();
+
   return (
-    <div className="flex justify-center items-center h-screen">Game page.</div>
+    <div className="flex flex-col items-center h-screen">
+      <div className="flex flex-row justify-between w-full p-5">
+        <div>{params.get("blueName")}</div>
+        <div>타이머</div>
+        <div>{params.get("redName")}</div>
+      </div>
+      <div>챔피언리스트</div>
+    </div>
   );
 }
