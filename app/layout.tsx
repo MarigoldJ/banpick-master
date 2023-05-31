e46192e67841461b4dce7596fabe1dc2934c252a
MarigoldJ <yusang0130@gmail.com>
2023-05-31 15:21:12 +0900

    hotfix: request from https to http

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Banpick Master",
   description: "Banpick webpage",
 };
@@ -15,6 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
