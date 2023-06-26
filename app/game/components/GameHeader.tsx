5aa5e43cd9b26141014332072467ca48c3e7c9dc
MarigoldJ <yusang0130@gmail.com>
2023-06-26 17:22:48 +0900

    feat: hide timer if banpick is not on-going

--- app/game/components/GameHeader.tsx
+++ app/game/components/GameHeader.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import useGame from "@/app/hooks/useGame";
 import PhaseCounter from "./header/PhaseCounter";
 import Timer from "./header/Timer";
 
@@ -7,11 +10,18 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ blueName, redName }) => {
+  const phase = useGame((state) => state.game.phase);
+
   return (
     <div className="flex flex-row justify-between w-full p-5">
       <div className="text-3xl font-bold text-blue-800">{blueName}</div>
 
-      <div className="flex flex-col items-center">
+      <div
+        className={`
+        flex flex-col items-center
+        ${1 <= phase && phase <= 20 ? "visible" : "invisible"}
+      `}
+      >
         <PhaseCounter />
         <Timer startSeconds={30} />
       </div>
