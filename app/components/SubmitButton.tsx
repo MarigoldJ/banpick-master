42e577c9c29929faf9d076e0f7aec4d56a049618
MarigoldJ <yusang0130@gmail.com>
2023-06-26 17:50:25 +0900

    fix: change css, not use w-full

--- app/components/SubmitButton.tsx
+++ app/components/SubmitButton.tsx
@@ -8,8 +8,8 @@ const SubmitButton: React.FC<SubmitButtonProps> = ({ label, onClick }) => {
     <button
       onClick={onClick}
       className={`
-    rounded-lg hover:opacity-70 transition w-full
-    bg-neutral-400
+    rounded-lg hover:opacity-70 transition py-2 px-10
+    bg-neutral-300
     border-neutral-500 border-2
     text-black font-semibold
   `}
